f3729c0e9f7680b2c04787c443f09984fc67a1cb
CatarinaMatos <aluno25976@ipt.pt>
2025-05-21 11:32:06 +0100

    tarefas
    criação de um To-do list

--- ProjetoDW/ProjetoDW/Controllers/TarefaController.cs
+++ ProjetoDW/ProjetoDW/Controllers/TarefaController.cs
@@ -3,133 +3,105 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProjetoDW.Data;
 using ProjetoDW.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ProjetoDW.Controllers
 {
+    [Authorize]
     public class TarefaController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public TarefaController(ApplicationDbContext context)
+        public TarefaController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Tarefa
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Tarefa.ToListAsync());
-        }
+            var userId = _userManager.GetUserId(User);
 
-        // GET: Tarefa/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            var tarefas = await _context.Tarefa
+                .Where(t => t.UtilizadorId == userId)
+                .ToListAsync();
 
-            var tarefa = await _context.Tarefa
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (tarefa == null)
-            {
-                return NotFound();
-            }
+            ViewBag.Tarefas = tarefas;
 
-            return View(tarefa);
-        }
-
-        // GET: Tarefa/Create
-        public IActionResult Create()
-        {
             return View();
         }
 
         // POST: Tarefa/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Terminado")] Tarefa tarefa)
+        public async Task<IActionResult> Create(string nome)
         {
-            if (ModelState.IsValid)
+            if (!string.IsNullOrWhiteSpace(nome))
             {
-                _context.Add(tarefa);
+                var userId = _userManager.GetUserId(User);
+
+                var tarefa = new Tarefa
+                {
+                    Nome = nome,
+                    Terminado = false,
+                    UtilizadorId = userId
+                };
+
+                _context.Tarefa.Add(tarefa);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
             }
-            return View(tarefa);
+
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: Tarefa/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var tarefa = await _context.Tarefa.FindAsync(id);
-            if (tarefa == null)
-            {
-                return NotFound();
-            }
+            if (tarefa == null || tarefa.UtilizadorId != _userManager.GetUserId(User))
+                return Unauthorized();
+
             return View(tarefa);
         }
 
         // POST: Tarefa/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Terminado")] Tarefa tarefa)
         {
-            if (id != tarefa.Id)
-            {
-                return NotFound();
-            }
+            if (id != tarefa.Id) return NotFound();
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(tarefa);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TarefaExists(tarefa.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(tarefa);
+            var userId = _userManager.GetUserId(User);
+            var tarefaExistente = await _context.Tarefa.FirstOrDefaultAsync(t => t.Id == id && t.UtilizadorId == userId);
+
+            if (tarefaExistente == null)
+                return Unauthorized();
+
+            tarefaExistente.Nome = tarefa.Nome;
+            tarefaExistente.Terminado = tarefa.Terminado;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: Tarefa/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var tarefa = await _context.Tarefa
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (tarefa == null)
-            {
-                return NotFound();
-            }
+            var userId = _userManager.GetUserId(User);
+            var tarefa = await _context.Tarefa.FirstOrDefaultAsync(t => t.Id == id && t.UtilizadorId == userId);
+
+            if (tarefa == null) return Unauthorized();
 
             return View(tarefa);
         }
@@ -139,13 +111,15 @@ namespace ProjetoDW.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var tarefa = await _context.Tarefa.FindAsync(id);
-            if (tarefa != null)
-            {
-                _context.Tarefa.Remove(tarefa);
-            }
+            var userId = _userManager.GetUserId(User);
+            var tarefa = await _context.Tarefa.FirstOrDefaultAsync(t => t.Id == id && t.UtilizadorId == userId);
 
+            if (tarefa == null)
+                return Unauthorized();
+
+            _context.Tarefa.Remove(tarefa);
             await _context.SaveChangesAsync();
+
             return RedirectToAction(nameof(Index));
         }
 
