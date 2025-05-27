7f02c48edfbd4274efdfbe16c445a6817cd7777e
CatarinaMatos <aluno25976@ipt.pt>
2025-05-27 16:11:00 +0100

    CategoriasController e views

---
+++ ProjetoDW/ProjetoDW/Controllers/CategoriasController.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetoDW.Data;
+using ProjetoDW.Models;
+
+namespace ProjetoDW.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categorias
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Categorias.Include(c => c.Utilizador);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Categorias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categorias = await _context.Categorias
+                .Include(c => c.Utilizador)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categorias == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorias);
+        }
+
+        // GET: Categorias/Create
+        public IActionResult Create()
+        {
+            ViewData["UtilizadoresFk"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Categorias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UtilizadoresFk,Tipo,Nome")] Categorias categorias)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categorias);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UtilizadoresFk"] = new SelectList(_context.Users, "Id", "Id", categorias.UtilizadoresFk);
+            return View(categorias);
+        }
+
+        // GET: Categorias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categorias = await _context.Categorias.FindAsync(id);
+            if (categorias == null)
+            {
+                return NotFound();
+            }
+            ViewData["UtilizadoresFk"] = new SelectList(_context.Users, "Id", "Id", categorias.UtilizadoresFk);
+            return View(categorias);
+        }
+
+        // POST: Categorias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UtilizadoresFk,Tipo,Nome")] Categorias categorias)
+        {
+            if (id != categorias.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categorias);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoriasExists(categorias.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UtilizadoresFk"] = new SelectList(_context.Users, "Id", "Id", categorias.UtilizadoresFk);
+            return View(categorias);
+        }
+
+        // GET: Categorias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categorias = await _context.Categorias
+                .Include(c => c.Utilizador)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categorias == null)
+            {
+                return NotFound();
+            }
+
+            return View(categorias);
+        }
+
+        // POST: Categorias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var categorias = await _context.Categorias.FindAsync(id);
+            if (categorias != null)
+            {
+                _context.Categorias.Remove(categorias);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoriasExists(int id)
+        {
+            return _context.Categorias.Any(e => e.Id == id);
+        }
+    }
+}
